c286f77d3f1f030e5b7523306de9cfbb40987800
MARUF3470 <smmaruf25@gmail.com>
2022-11-10 01:00:38 +0600

    review added

--- src/component/pages/post/AddPost.js
+++ src/component/pages/post/AddPost.js
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const AddPost = () => {
     const { user } = useContext(AuthContext)
     // console.log(user)
+    const displayName = user?.displayName;
+    const email = user?.email;
+    const photoURL = user?.photoURL;
+
     const id = window.location.pathname.split('/')[2];
     console.log(id)
-    const { displayName, email, photoURL } = user
     const handleReview = event => {
         event.preventDefault()
         const review = event.target.review.value;
@@ -17,10 +21,30 @@ const AddPost = () => {
             serviceId: id,
             review: review
         }
-        fetch()
+        console.log(reviewDetails)
+        fetch('http://localhost:5000/review', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(reviewDetails)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    toast.success('Review added')
+                    event.target.reset()
+                }
+            })
+            .catch(err => console.log(err))
     }
     return (
         <form className='w-3/4 mx-auto my-10' onSubmit={handleReview}>
+            <Toaster
+                position="top-center"
+                reverseOrder={false}
+            />
             <textarea name='review' className="textarea textarea-primary w-full h-80 mx-auto" placeholder="add review" required></textarea>
             <div className='text-center'>
                 <input type="submit" className='btn btn-wide btn-sm' value="Submit" />
